8ac454802e67f9a186d9244159d800b83cc18e22
DESKTOP-MAF5T6N\Darky <anandarizky166@gmail.com>
2024-02-28 10:47:13 +0700

    update quantity by product

--- store/carts.js
+++ store/carts.js
@@ -3,20 +3,34 @@ export const state = () => ({
   items: [],
 })
 
-// Buat mutation yang dilakukan untuk menambahkan id dan quantity pada state items
+// Buat mutation
 export const mutations = {
+  // Buat mutation yang dilakukan untuk menambahkan id dan quantity pada state items
   addItem(state, id) {
     state.items.push({
       id: id,
       quantity: 1,
     })
-    console.log(state.items)
+  },
+  // Buat mutation untuk menambahkan quantity jika sudah ada id yang sama pada state items
+  incrementItem(state, id) {
+    state.items.find((item) => item.id === id).quantity++
   },
 }
 
 // Membuat action berdasarkan mutation dengan membawa id yang di klik pada card
 export const actions = {
-  addToCart({ commit }, id) {
-    commit('addItem', id)
+  addToCart({ commit, state }, id) {
+    // Mencocokan id pada state items dan pada id yang di klik
+    const found = state.items.find((item) => item.id === id)
+    // Jika ada
+    if (found) {
+      // Commit mutation tambah quantity
+      commit('incrementItem', id)
+    }
+    // Commit mutation tambah item to cart
+    else {
+      commit('addItem', id)
+    }
   },
 }
